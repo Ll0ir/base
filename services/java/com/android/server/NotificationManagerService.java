a14bcb65ba8fee6e5d9fc64321b6183a35406ff4
Robert Shannon <rob_shannon539@hotmail.com>
2012-10-26 06:00:53 -0700

    Do pulse LED on new notification when the screen is turned off.
    If a new notification with LED pulse information is occurred
    and the screen is turned off then the LED should be pulsed
    until the lock screen is swiped off.
    
    Change-Id: Id90d0dcd42ca0c4dadd131f6b4990d2790b402fb
    Signed-off-by: Soowon Choi <choi.soowon@gmail.com>
    
    Conflicts:
    
    	services/java/com/android/server/NotificationManagerService.java
    
    	modified:   services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -580,6 +580,7 @@ public class NotificationManagerService extends INotificationManager.Stub
                 mScreenOn = false;
                 mWasScreenOn = true;
                 updateLightsLocked();
+                updateNotificationPulse();
             } else if (action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
                 mInCall = (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
                         TelephonyManager.EXTRA_STATE_OFFHOOK));
